a5aa33e53dbfeec76ede719d2842575581af95e7
Chrstphrjsn <cjason.rosales@gmail.com>
2023-05-30 18:06:50 +0300

    Added Common Boilerplate

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const isDev = true;
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600
-  })
+    width: isDev ? 1200 : 600,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
-  win.loadFile('./renderer/index.html')
-}
+  if (isDev) {
+    win.webContents.openDevTools();
+  }
+
+  win.loadFile(path.join(__dirname, "./renderer/index.html"));
+};
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
-  app.on('activate', () => {
+  app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+      createWindow();
     }
-  })
-})
+  });
+});
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
